80fc5c7975dbc50ff49579e97eaa2d39433dc3d9
archefnx <artykbaiev0000@gmail.com>
2021-10-28 00:38:20 +0600

    Added a full Russian-English dictionary.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let words = null;
 $.ajax({
     'async': false,
     'global': false,
-    'url': "json/words-rus-eng.json",
+    'url': "json/word-rus-eng.json",
     'dataType': "json",
     'success': function (data) {
         words = data;
@@ -14,7 +14,7 @@ $.ajax({
 });
 
 $('#next-button').on('click', function() {
-    let randNum = randomInt(20);
+    let randNum = randomInt(3112);
 
     $('#word').text(words['list'][randNum]['word']);
     $('#translation').text(words['list'][randNum]['translation']);
@@ -24,7 +24,7 @@ $(window).keypress(function (e) {
     if (e.key === ' ' || e.key === 'Spacebar') {
         e.preventDefault()
 
-        let randNum = randomInt(20);
+        let randNum = randomInt(3112);
 
         $('#word').text(words['list'][randNum]['word']);
         $('#translation').text(words['list'][randNum]['translation']);
